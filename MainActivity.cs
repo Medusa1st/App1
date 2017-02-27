328fb6bf7eb0a676dfab9e5cf6a02d84c15e0e45
王宇伟 <littlemeidusha@163.com>
2017-02-27 20:43:58 +0800

    First Build

---
+++ MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    // 更改Label的值可更改APP名称和APP内标题
+    [Activity(Label = "小安琪成长记", MainLauncher = true, Icon = "@drawable/aq")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("我们的小安琪{0}岁啦!", count++); };
+        }
+    }
+}
+
